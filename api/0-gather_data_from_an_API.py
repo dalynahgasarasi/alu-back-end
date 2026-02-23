7349dd5310d986fda10603b54db4ff5951cb769a
dalynahgasarasi <d.gasarasi@alustudent.com>
2026-02-23 08:20:03 -0800

    File modifications

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,17 +3,27 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    url = "https://jsonplaceholder.typicode.com/users/{}"
-    user = requests.get(url.format(employee_id)).json()
-    url2 = "https://jsonplaceholder.typicode.com/todos?userId={}"
-    todos = requests.get(url2.format(employee_id)).json()
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(employee_id)).json()
+
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(employee_id)).json()
+
     employee_name = user.get("name")
-    done_tasks = [task for task in todos if task.get("completed") is True]
     total = len(todos)
-    done = len(done_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, done, total))
+    done_tasks = [task for task in todos if task.get("completed")]
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, len(done_tasks), total))
+
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
