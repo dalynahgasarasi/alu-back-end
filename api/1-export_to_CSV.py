36329ae353e4332f359d3dd4c2cf883c197b0a95
dalynahgasarasi <d.gasarasi@alustudent.com>
2026-02-23 08:29:43 -0800

    CSV export file

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script that exports TODO list data to CSV format."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/users/{}"
+    user = requests.get(url.format(employee_id)).json()
+    url2 = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todos = requests.get(url2.format(employee_id)).json()
+    username = user.get("username")
+    with open("{}.csv".format(employee_id), "w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([
+                employee_id,
+                username,
+                task.get("completed"),
+                task.get("title")
+            ])
